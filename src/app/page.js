c3f4669e53e71095b3bc8fbd21664763ee9bd5c6
anamul01988 <anamulhaque0198877@gmail.com>
2024-08-03 17:39:07 +0600

    home banner section is added

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,15 @@
 // "use client";
-// import { Button } from "@material-tailwind/react";
-import { Button } from "./MTailwind";
-import Image from "next/image";
 
-export default function Home() {
-  return <div style={{ height: " 100vh", backgroundColor: "#fff" }}></div>;
+
+import Home from "./pages/home";
+
+export default function MainPage() {
+  return (
+    <div
+      className="mx-auto"
+      style={{ height: "auto", backgroundColor: "#fff" }}
+    >
+      <Home />
+    </div>
+  );
 }
