29965c33722d73b205e58508d64fe8defd975513
anamul01988 <anamulhaque0198877@gmail.com>
2024-10-27 00:29:37 +0600

    checkout page done

--- src/app/placeOrder/page.js
+++ src/app/placeOrder/page.js
@@ -1,13 +1,13 @@
 // "use client";
 
-import PlaceOrder from "../../components/pages/PlaceOrder";
+// import PlaceOrder from "../../components/pages/PlaceOrder";
+import Checkout from "../../components/pages/PlaceOrder/Checkout";
 
 export default function Page() {
   return (
-    <div
-      className="mx-auto"
-    >
-      <PlaceOrder />
+    <div className="mx-auto">
+      {/* <PlaceOrder /> */}
+      <Checkout />
     </div>
   );
 }
